c631b3808bedd83da5169532d6719df858562500
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-12 19:59:50 -0300

    feat(general): add security config class

--- src/main/java/rafaelandrade/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/rafaelandrade/libraryapi/config/SecurityConfiguration.java
@@ -2,9 +2,16 @@ package rafaelandrade.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +21,46 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
                 .formLogin(configurer -> {
-                    configurer.loginPage("/login").permitAll();
+                    configurer.loginPage("/login");
                 })
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/autores/**").hasAuthority("CADASTRAR_AUTOR");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.POST, "/livros/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/livros/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/livros/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/livros/**").hasAnyRole("USER", "ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/autores/**").hasAnyRole("USER", "ADMIN");
                     authorize.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(passwordEncoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("321"))
+                .authorities("CADASTRAR_AUTOR")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
